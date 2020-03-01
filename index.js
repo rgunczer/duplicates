8473b329acd19ce25680475986bebd197a90dd02
rgunczer <rgunczer@gmail.com>
2020-03-01 18:18:46 +0100

    add measurement

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ const extension = '.pdf';
 const msg = `Discovering files in [${pathToRoot}] with extension [${extension}]`;
 const spinner = ora(msg).start();
 
+const hrstart = process.hrtime();
+
 walker(pathToRoot, extension)
     .then(fileList => {
         spinner.text = 'Finding duplicates';
         spinner.render();
 
-        findDuplicatesUsingFiltering(fileList)
+        const prom1 = findDuplicatesUsingFiltering(fileList);
+        const prom2 = findDuplicatesUsingSingleLoop(fileList);
+
+        Promise.all([prom1, prom2])
             .then(duplicates => {
                 spinner.text = 'Done';
                 spinner.render();
@@ -30,14 +35,6 @@ walker(pathToRoot, extension)
                 console.log(`files: [${fileList.length}]`);
                 console.log(`dupliacates: [${duplicates.length}]`);
 
-                spinner.stop();
-            });
-
-        findDuplicatesUsingSingleLoop(fileList)
-            .then(duplicates => {
-                spinner.text = 'Done';
-                spinner.render();
-
                 console.log('\n\nDuplicates [Loop]:\n\n', duplicates);
 
                 console.log('\n---------------')
@@ -45,6 +42,9 @@ walker(pathToRoot, extension)
                 console.log(`dupliacates: [${duplicates.length}]`);
 
                 spinner.stop();
+
+                const hrend = process.hrtime(hrstart);
+                console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
             });
     });
 
